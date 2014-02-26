2fb836df82f8e340c3890124d067a8aa897a9a7f
Nirajan Singh <singh.nirajan@gmail.com>
2014-02-27 01:19:27 +0530

    Project added

---
+++ Aprimo.Utility.Web/Scripts/custom/apps/app.js
@@ -0,0 +1,18 @@
+﻿(function (window, angular, undefined) {
+    'use strict';
+    var mainApp = angular.module('mainApp', ['ngRoute', 'mainController']);
+
+    mainApp.config(['$routeProvider', aprimoRouter]);
+
+    var aprimoRouter = function ($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'Views/Home/home.html',
+            controller: 'homeCtrl'
+        }).when('/login', {
+            templateUrl: 'Views/Home/home.html',
+            controller: 'homeCtrl'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    };
+});
