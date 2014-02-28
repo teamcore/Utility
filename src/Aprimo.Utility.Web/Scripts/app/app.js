a296fba6cb15e10a620ddf366057cbec09dbeae8
Nirajan Singh <singh.nirajan@gmail.com>
2014-03-01 02:54:12 +0530

    in progress

--- src/Aprimo.Utility.Web/Scripts/app/app.js
+++ src/Aprimo.Utility.Web/Scripts/app/app.js
@@ -4,6 +4,7 @@
     var appModule = angular.module('mainApp', ['ngRoute', 'ngResource', 'ui.bootstrap']);
     appModule.config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/', { templateUrl: 'Html/dashboard.html', controller: 'dashboardController' });
+        $routeProvider.when('/login', { templateUrl: 'Html/login.html', controller: 'loginController' });
         $routeProvider.otherwise({ redirectTo: '/' });
     }]);
 
---
+++ src/Aprimo.Utility.Web/Scripts/app/controllers/loginController.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    'use strict';
+    var appModule = angular.module('mainApp');
+    appModule.controller('loginController', ['$scope',
+        function loginController($scope) {
+            $scope.Message = "";
+
+            $scope.init = function () {
+                $scope.Message = "Login Page";
+            };
+
+            $scope.init();
+
+        }]);
+
+}());
