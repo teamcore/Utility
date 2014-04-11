a710badfc3071236cbe36fe836d1bde29d6a0602
Nirajan Singh <singh.nirajan@gmail.com>
2014-04-11 14:28:16 +0530

    Adding Mvc Pages

---
+++ src/Ns.Utility.Web/Controllers/GroupController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ns.Utility.Web.Controllers
+{
+    public class GroupController : Controller
+    {
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        public ActionResult AddEdit()
+        {
+            return View();
+        }
+	}
+}
