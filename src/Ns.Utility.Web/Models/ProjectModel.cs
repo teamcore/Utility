554bc4079d4dcd86ab9d9b7a3d7a085480710735
Nirajan Singh <singh.nirajan@gmail.com>
2014-04-13 03:32:39 +0530

    Validation on project master

--- src/Ns.Utility.Web/Models/ProjectModel.cs
+++ src/Ns.Utility.Web/Models/ProjectModel.cs
@@ -1,6 +1,7 @@
 ﻿using Ns.Utility.Framework.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,8 +9,10 @@ namespace Ns.Utility.Web.Models
 {
     public class ProjectModel : BaseEntityModel
     {
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Code { get; set; }
 
         public string Description { get; set; }
